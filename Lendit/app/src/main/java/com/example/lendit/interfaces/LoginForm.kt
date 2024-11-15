6d30f52ab4ce47dd1ea89e009d9b0ceab3112f30
miguel-loureiro <miguel.silvaloureiro@gmail.com>
2024-11-15 18:53:35 +0000

    add login field initial

--- Lendit/app/src/main/java/com/example/lendit/interfaces/LoginForm.kt
+++ Lendit/app/src/main/java/com/example/lendit/interfaces/LoginForm.kt
@@ -1,11 +1,18 @@
 package com.example.lendit.interfaces
 
+import android.R.attr.contentDescription
+import android.R.attr.tint
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -14,14 +21,43 @@ import com.example.lendit.ui.theme.LenditTheme
 @Composable
 fun LoginForm(){
     Surface{
-        Column{
-            /*LoginField()
-            PasswordField()
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .statusBarsPadding()
+        ){
+            LoginField(
+                value = "data",
+                onChange = {}
+            )
+            /*PasswordField()
             LabeledCheckbox()
             Button(onClick = { *//*TODO *//* }) {
              } */
         }
     }
+}
+
+@Composable
+fun LoginField(
+    value: String,
+    onChange: (String) ->Unit,
+    modifier: Modifier = Modifier
+) {
+    val leadingIcon = @Composable{
+        Icon(
+            Icons.Default.Person,
+            contentDescription = "",
+            tint = MaterialTheme.colorScheme.primary
+        )
+    }
+
+    TextField(
+        value = value,
+        onValueChange = onChange,
+        modifier = modifier,
+        leadingIcon = leadingIcon
+    )
 }
 
 @Preview(showBackground = true, device = "id:Nexus One", showSystemUi = true, apiLevel = 34)
