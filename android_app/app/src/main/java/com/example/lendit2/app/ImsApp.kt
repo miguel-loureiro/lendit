f9d0262686288d65b1e73234adf4b7ff934f8011
miguel-loureiro <miguel.silvaloureiro@gmail.com>
2024-11-16 12:03:37 +0000

    new ims app project layout

---
+++ android_app/app/src/main/java/com/example/lendit2/app/ImsApp.kt
@@ -0,0 +1,16 @@
+package com.example.lendit2.app
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun ImsApp(){
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = Color.White
+        ) {
+    }
+}
