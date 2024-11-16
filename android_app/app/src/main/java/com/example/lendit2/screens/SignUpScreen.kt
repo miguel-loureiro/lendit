1bd2ae81b3af93c474c9e1a44c74f49bb930ca27
miguel-loureiro <miguel.silvaloureiro@gmail.com>
2024-11-16 18:18:06 +0000

    title and sub title added

---
+++ android_app/app/src/main/java/com/example/lendit2/screens/SignUpScreen.kt
@@ -0,0 +1,38 @@
+package com.example.lendit2.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.lendit2.components.NormalTextComponent
+import com.example.lendit2.R
+import com.example.lendit2.components.HeadingTextComponent
+
+@Composable
+fun SignUpScreen() {
+    Surface(
+        color = Color.White,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+            .padding(28.dp)
+    ) {
+        Column(modifier = Modifier.fillMaxSize()){
+            NormalTextComponent(value = stringResource(id = R.string.hello))
+            HeadingTextComponent(value = stringResource(id = R.string.create_account))
+        }
+    }
+}
+
+@Preview
+@Composable
+fun DefaultPreviewSignUpScreen(){
+    SignUpScreen()
+}
